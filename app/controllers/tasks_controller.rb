9b57f8c32e4d86e1a1de4af90ea22ebdb7b6113b
Will Thompson <will.f.thompson@gmail.com>
2020-08-18 13:45:12 +0200

    working crud actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @new_task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    Task.create(params.require("task").permit(:title, :details))
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(params.require("task").permit(:title, :details, :completed))
+    redirect_to tasks_path
+  end
+
+  def delete
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path
+  end
+end
